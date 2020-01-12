ba4fcebeebd46afee0659ed20245f3620b808ffd
Alexander Zhukov <36170455+adzhukov@users.noreply.github.com>
2020-01-12 22:53:26 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# encoding: utf-8
+from distutils.core import setup, Extension
+
+module = Extension('macos_notifications', sources = ['notify.c'], extra_compile_args = ["-ObjC"], extra_link_args = ["-framework", "Foundation"])
+
+setup(name='macos-notifications',
+      version = '1.0',
+      description = 'macOS package for sending notifications',
+      python_requires = '>=3',
+      platforms = ['MacOS X'],
+      ext_modules = [module])
